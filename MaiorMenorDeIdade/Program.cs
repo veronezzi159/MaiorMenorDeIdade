4cdd05129f2c71c1fe2c0d467faeff072f67937b
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 00:58:21 -0300

    Add project files.

---
+++ MaiorMenorDeIdade/Program.cs
@@ -0,0 +1,16 @@
+﻿int idade;
+
+Console.WriteLine("Digite sua idade");
+idade = int.Parse(Console.ReadLine());
+
+if (idade >= 0)
+{
+    if (idade < 18)
+    {
+        Console.WriteLine("\n Menor de idade");
+    } 
+    else
+    {
+        Console.WriteLine("\n Maior de idade");
+    }
+}
